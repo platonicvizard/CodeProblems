76562143db06ed015dc64cfc43d0c202738c7e26
dlfrontend <dlfrontend@outlook.com>
2019-05-14 05:47:45 -0400

    initial commit

---
+++ findIndicesOfTwoSums.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function findIndicesOfTwoSums(nums, target) {
+ 
+}
+
+var result = findIndicesOfTwoSums([2, 7, 11, 15], 22);
+
+console.log(result);
